02b77e8705d1c8166adbe7aa743d458cc8421c51
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2024-08-30 13:37:07 -0300

    User registration added

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -25,7 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
+    "users.apps.UsersConfig",
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -110,3 +113,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
