82bd9d879a046905099798884c1a95ee559d314f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2024-08-31 17:01:50 -0300

    User Profile and Picture configuration (added)

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,6 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
